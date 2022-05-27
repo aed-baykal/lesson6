c939aa3aada0836ebd6c9c28973ac202c63d6d0e
Andrey <aedbaykal@gmail.com>
2022-05-27 23:35:00 +0300

    Д/З к шестому уроку.

--- src/main/java/ru/gb/HelloServlet.java
+++ src/main/java/ru/gb/HelloServlet.java
@@ -9,5 +9,8 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().printf("<html><body>");
+        resp.getWriter().printf("<h1>" + "HelloWorld" + "</h1>");
+        resp.getWriter().printf("</body></html>");
     }
 }
